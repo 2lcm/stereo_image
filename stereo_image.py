f7b3bc69693958bf4e77593f23b39f176c8e09b4
Lee Chul Min <chul0329@kaist.ac.kr>
2018-10-24 15:48:29 +0900

    no threshold, image filter pre-processing, not good :( 1. change appropriate point abs(difference) -> histogram 2. Do not use pillow just numpy for faster
    But, I change my topic.. Wiener Deconvolution.. finish last time

--- stereo_image.py
+++ stereo_image.py
@@ -7,10 +7,23 @@ import numpy as np
 # It needs '.'
 org_dir = "./All-2views"
 res_dir = "./my_result"
-window_width = 7
+window_width = 5
+
+all_filters = [
+    ImageFilter.BLUR,
+    ImageFilter.CONTOUR,
+    ImageFilter.DETAIL,
+    ImageFilter.EDGE_ENHANCE,
+    ImageFilter.EDGE_ENHANCE_MORE,
+    ImageFilter.EMBOSS,
+    ImageFilter.FIND_EDGES,
+    ImageFilter.SHARPEN,
+    ImageFilter.SMOOTH,
+    ImageFilter.SMOOTH_MORE,
+]
 
 
-def myfunc(img1, img2):
+def myfunc(img1, img2, threshold):
     # img1 - left view, img2 - right view
     w, h = img1.size
     pw = window_width // 2
@@ -19,10 +32,12 @@ def myfunc(img1, img2):
     for j in range(h-window_width):  # height
         if j % 10 == 0:
             print("j=", j)
+        # hit = 0
+        # miss = 0
         for i in range(w-window_width):  # width
             max_d = None
             val = 0
-            for t in range(w-window_width):  # along X-axis
+            for t in range(i+1):  # along X-axis
                 # calculate sum of difference in window
                 d = 0
                 for a in range(window_width):
@@ -32,16 +47,23 @@ def myfunc(img1, img2):
                             px2 = img2.getpixel((t+a, j+b))
                             tmp = 0
                             for index in range(len(px1)):
-                                tmp += (px1[index] - px2[index]) ** 2
+                                tmp += abs((px1[index] - px2[index]))
+                                # tmp += (px1[index] - px2[index])**2
                             arr[j+b][i+a][t+a] = tmp
+                            # miss += 1
+                        # else:
+                        #     hit += 1
                         d += arr[j+b][i+a][t+a]
 
                 if max_d is None or max_d > d:
                     max_d = d
                     val = i-t
-            if i-1 >= 0 and res_img.getpixel((i-1, j)) - (64+2*val) < 10:
-                res_img.putpixel((i-1, j), 64 + 2 * val)
-            res_img.putpixel((i, j), 64+2*val)
+            val = 4*(val // 4)
+            # if i-1 >= 0 and res_img.getpixel((i-1, j)) - (64+2*val) < threshold:
+            #     res_img.putpixel((i-1, j), 64 + val)
+            res_img.putpixel((i, j), 64+val)
+        # print("hit:", hit)
+        # print("miss:", miss)
     return res_img
 
 
@@ -54,20 +76,26 @@ if __name__ == "__main__":
 
         img1 = pad_image(image1, window_width // 2)  # reflective padding
         img2 = pad_image(image2, window_width // 2)
-        img1_blur = img1.filter(ImageFilter.BLUR)
-        img2_blur = img2.filter(ImageFilter.BLUR)
 
-        res_image = myfunc(img1, img2)
-        # res_image.show()
-        res_image.save(res_dir + "/" + cur + "/res5.png")
+        # for itr in range(0,101,2):
+        #     res_image = myfunc(img1, img2, itr)
+        #     # res_image.show()
+        #     res_image.save(res_dir + "/" + cur + "/res_"+str(itr)+".png")
 
-        res_image_blur = myfunc(img1_blur, img2_blur)
-        res_image_blur.save(res_dir + "/" + cur + "/res_blur.png")
+        res_image = myfunc(img1, img2, 0)
+        res_image.save(res_dir + "/" + cur + "/res_abs.png")
+
+        for flt in all_filters:
+            img1_filter = img1.filter(flt)
+            img2_filter = img2.filter(flt)
+            file_name = str(flt).split(".")[2].split("'")[0]
+            res_image_filter = myfunc(img1_filter, img2_filter, None)
+            res_image_filter.save(res_dir + "/" + cur + "/" + file_name + ".png")
 
         # one_color_image = Image.new('L', image1.size, 64)
         # one_color_image.save('64.png')
 
         # image1.save(res_dir + "/" + cur + "/view1.png")
         # image2.save(res_dir + "/" + cur + "/view5.png")
-        # break
+        break
 
